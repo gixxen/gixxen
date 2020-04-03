dda925fe413fc57647d12870e774f844ab767f0c
gixxen <63117175+gixxen@users.noreply.github.com>
2020-04-03 12:42:48 -0400

    Add files via upload

---
+++ imageEffect.js
@@ -0,0 +1,12 @@
+function animate() {
+    document.querySelectorAll('.slideDown').forEach(element => {
+        element.classList.add('slideDownActive');
+    });
+    document.querySelectorAll('.slideUp').forEach(element => {
+        element.classList.add('slideUpActive');
+    });
+}
+
+
+
+  
---
+++ menuEffect.js
@@ -0,0 +1,42 @@
+var buttons = document.querySelectorAll('.button');
+var effects = document.querySelectorAll('.effect');
+
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].effect = effects[i];
+    buttons[i].addEventListener('mouseenter', hoverEffect);
+    buttons[i].addEventListener('mouseleave', hoverEffectOut);
+    buttons[i].addEventListener('click', effectClick);
+
+    function effectClick(event) {
+        var target = event.target;
+        var hover = target.effect;
+        if (document.querySelectorAll('.effectClick').length > 0) {
+            document.querySelectorAll('.effectClick').forEach(element => {
+                element.classList.remove('effectClick');
+            });
+        }
+        hover.classList.add('effectClick');
+        hover.classList.remove('effectActive', 'effectActiveOut');
+    }
+    function hoverEffect(event) {
+        var target = event.target;
+        var hover = target.effect;
+        if (hover.classList.contains('effectClick')) {
+
+        } else {
+            hover.classList.remove('effectActiveOut');
+            hover.classList.add('effectActive');
+        }
+    }
+    function hoverEffectOut(event) {
+        var target = event.target;
+        var hover = target.effect;
+        if (hover.classList.contains('effectClick')) {
+            
+        } else {
+            hover.classList.remove('effectActive');
+            hover.classList.add('effectActiveOut');
+        }
+    }
+}
+
